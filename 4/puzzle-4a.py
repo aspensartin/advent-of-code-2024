2acc335c5db02905724b3263dbf731f73743bc6e
aspensartin <as30g20@soton.ac.uk>
2024-12-04 16:13:31 +0000

    my stupid chud solution

--- 4/puzzle-4a.py
+++ 4/puzzle-4a.py
@@ -1,23 +1,46 @@
 import numpy as np
 
-with open('test-input.txt', 'r') as input:
+with open('input.txt', 'r') as input:
     data = input.read()
     input.close()
 
 lines = data.split('\n')
 chars = np.array([list(i) for i in lines])
-words = [] 
+words = []
+xmas = 0
 
-for i in range(len(chars[:, 0])):
-    for j in range(len(chars[0, :])):
-        if j < len(chars[:, 0])-3:
-            words.append(chars[i,j]+chars[i,j+1]+chars[i,j+2]+chars[1,j+3])
-        if i < len(chars[0, :])-3:
-            words.append(chars[i,j]+chars[i+1,j]+chars[i+2,j]+chars[i+3,j])
-        if i < len(chars[0, :])-3 and j < len(chars[:, 0])-3: 
-            words.append(chars[i,j]+chars[i+1,j+1]+chars[i+2,j+2]+chars[i+3,j+3])
-        if i >= 3 and j < len(chars[0, :])-3:
-            words.append(chars[i,j]+chars[i-1,j+1]+chars[i-2,j+2]+chars[i-3,j+3])
+print(range(0, len(chars[3:, 0])))
 
-np.savetxt('words.txt', words, fmt='%s')
-print(words.count('XMAS')+words.count('SAMX'))
+#horizontal
+for i in range(0, len(chars[3:, 0])):
+    for j in range(0, len(chars[0, :])):
+        if chars[i,j] == 'X' and chars[i+1, j] == 'M' and chars[i+2, j] == 'A' and chars[i+3, j] == 'S':
+                xmas +=1
+        if chars[i,j] == 'S' and chars[i+1, j] == 'A' and chars[i+2, j] == 'M' and chars[i+3, j] == 'X':
+                xmas +=1
+
+#vertical
+for i in range(0, len(chars[0:, 0])):
+    for j in range(0, len(chars[0, 3:])):
+        if chars[i,j] == 'X' and chars[i, j+1] == 'M' and chars[i, j+2] == 'A' and chars[i, j+3] == 'S':
+            xmas +=1
+        if chars[i,j] == 'S' and chars[i, j+1] == 'A' and chars[i, j+2] == 'M' and chars[i, j+3] == 'X':
+            xmas +=1
+        
+#top-left -> bottom-right
+for i in range(0, len(chars[3:, 0])):
+    for j in range(0, len(chars[0, 3:])):
+        if chars[i,j] == 'X' and chars[i+1, j+1] == 'M' and chars[i+2, j+2] == 'A' and chars[i+3, j+3] == 'S':
+            xmas +=1
+        if chars[i,j] == 'S' and chars[i+1, j+1] == 'A' and chars[i+2, j+2] == 'M' and chars[i+3, j+3] == 'X':
+            xmas +=1
+
+#top-right -> bottom-left
+for i in range(3, len(chars[:, 0])):
+    for j in range(0, len(chars[0, 3:])):
+        if chars[i,j] == 'X' and chars[i-1, j+1] == 'M' and chars[i-2, j+2] == 'A' and chars[i-3, j+3] == 'S':
+            xmas +=1
+        if chars[i,j] == 'S' and chars[i-1, j+1] == 'A' and chars[i-2, j+2] == 'M' and chars[i-3, j+3] == 'X':
+            xmas +=1       
+
+print(xmas, 'matches for xmas')
