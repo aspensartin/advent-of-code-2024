e310ed8ce7719dfe4dfc2263b49de18405bf0081
aspensartin <as30g20@soton.ac.uk>
2024-12-04 15:15:11 +0000

    whuh huh

--- 4/puzzle-4a.py
+++ 4/puzzle-4a.py
@@ -1,18 +1,24 @@
 import numpy as np
 
-with open('input.txt', 'r') as input:
+with open('test-input.txt', 'r') as input:
     data = input.read()
     input.close()
 
 lines = data.split('\n')
 chars = np.array([list(i) for i in lines])
-words = []
+words = [] 
 
-for i in range(np.size(chars, 0)-3):
-    for j in range(np.size(chars, 1)-3):
-        words.append(chars[i,j+k]+chars[i+1,j+k]+chars[i+2,j+k]+chars[i+3,j+k] for k in range(0,3))
-        words.append(chars[i+k,j]+chars[i+k,j+1]+chars[i+k,j+2]+chars[1+k,j+3] for k in range(0,3))
-        words.append(chars[i,j]+chars[i+1,j+1]+chars[i+2,j+2]+chars[i+3,j+3])
-        words.append(chars[i+3,j]+chars[i+2,j+1]+chars[i+1,j+2]+chars[i,j+3])
-        
-print(words.count('XMAS') + words.count('SAMX'))
+for i in range(len(chars[:, 0])):
+    for j in range(len(chars[0, :])):
+        if j < len(chars[:, 0])-3:
+            words.append(chars[i,j]+chars[i,j+1]+chars[i,j+2]+chars[1,j+3])
+        if i < len(chars[0, :])-3:
+            words.append(chars[i,j]+chars[i+1,j]+chars[i+2,j]+chars[i+3,j])
+        if i < len(chars[0, :])-3 and j < len(chars[:, 0])-3: 
+            words.append(chars[i,j]+chars[i+1,j+1]+chars[i+2,j+2]+chars[i+3,j+3])
+        if i >= 3 and j < len(chars[0, :])-3:
+            words.append(chars[i,j]+chars[i-1,j+1]+chars[i-2,j+2]+chars[i-3,j+3])
+
+print(range(len(chars[:, 0])))
+np.savetxt('words.txt', words, fmt='%s')
+print(words.count('XMAS')+words.count('SAMX'))
