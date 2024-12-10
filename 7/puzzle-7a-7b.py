be5f2bd6756cefe41659f8aad6bfeb189b39e1ba
Aspen <aspen.sartin@gmail.com>
2024-12-10 15:35:54 +0000

    timer

--- 7/puzzle-7a-7b.py
+++ 7/puzzle-7a-7b.py
@@ -1,10 +1,12 @@
 import numpy as np
+import time
+t0 = time.time()
 
 with open('input.txt', 'r') as input:
     data = input.read()
     input.close()
 
-eqs = [[np.char.strip(i.split(' '), ':')] for i in data.split('\n')]
+eqs = [[np.char.strip(i.split(' '), ':')] for i in data.split('\n')][0]
 
 def add_or_mult(eqn):
     a = np.array(eqn[0]).flatten()
@@ -25,7 +27,7 @@ def add_mult_or_concat(eqn):
     if type(a) == int:
         output = [a+b, a*b, int(str(a)+str(b))]
     else:
-        output = np.array([[int(i)+b, int(i)*b, str(i)+str(b)] for i in a]).flatten()
+        output = np.array([[int(i)+b, int(i)*b, int(str(i)+str(b))] for i in a]).flatten()
     if len(eqn) == 2:
         return output
     eqn[1] = output
@@ -36,11 +38,12 @@ result_a = 0
 result_b = 0
 
 for i in eqs:
-    eqn = [int(j) for j in i[0][1:]]
-    ans = int(i[0][0])
+    eqn = [int(j) for j in i[1:]]
+    ans = int(i[0])
     if ans in add_or_mult(eqn):
         result_a += ans
     if ans in add_mult_or_concat(eqn):
         result_b += ans
 
 print('calibration result is', result_a, 'or', result_b, 'including concatenations')
+print('execution time (s):', time.time() - t0)
